f0ab35b35ad671cec68c51cf9c58de1d32b3dc68
tiamacservice <tiamacservice@gmail.com>
2023-12-12 06:14:53 +0700

    database

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,15 +1,9 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize(
-  "tiamdb_rememberto",
-  "tiamdb_rememberto",
-  "24f82d6234a6ec74d787b7d0c0c35ca57ed6bd18",
-  {
-    host: "v10.h.filess.io",
-    dialect: "mysql",
-    port: 3307,
-  }
-);
+const db = new Sequelize("tiamacse_tiamdb", "tiamacse_admin", "I#Gdcq)*9HOo", {
+  host: "tiamacservice.my.id",
+  dialect: "mysql",
+});
 
 // const db = new Sequelize('tiam_db', 'root','',{
 //     host:"localhost",
