4ddc18dc6cb2f3f99afc22f92567782f2e544047
Muhamad Pradesa <muhammad.pradesa@lincgrp.com>
2023-12-13 08:54:50 +0700

    baru

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 
+<<<<<<< HEAD
 // const db = new Sequelize(
 //   "tiamdb_rememberto",
 //   "tiamdb_rememberto",
@@ -14,6 +15,11 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize('tiam_db', 'root','',{
     host:"localhost",
     dialect:"mysql"
+=======
+const db = new Sequelize("tiamacse_tiamdb", "tiamacse_admin", "I#Gdcq)*9HOo", {
+  host: "tiamacservice.my.id",
+  dialect: "mysql",
+>>>>>>> f0ab35b35ad671cec68c51cf9c58de1d32b3dc68
 });
 
 export default db;
